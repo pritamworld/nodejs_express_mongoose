f45bfac1d1e6841b976510b52409ff5b52b4063b
moxDroid Labs Inc <pritam.world@gmail.com>
2022-10-14 20:17:55 -0400

    friday lab work

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
 const express = require("express")
 const booksRoutes = require("./routes/books")
+const mongoose = require("mongoose")
 
 const app = express()
 
 const SERVER_PORT = 3001
 
+const DB_URL = "mongodb+srv://sa:rlRFbMwWVXDNWMpt@cluster0.7wn4nmp.mongodb.net/db_f2022_comp3123?retryWrites=true&w=majority"
+
+mongoose.connect(DB_URL, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+})
+
 app.use(express.json())
 app.use(express.urlencoded())
 
+app.use("/library/", booksRoutes)
 
-app.use("/books", booksRoutes)
+
 
 app.route("/")
     .get((req, res) => {
