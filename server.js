dba48d7dc09cf4f47cec3623d87acfd32105e3e3
Meness428 <68751841+Meness428@users.noreply.github.com>
2022-10-18 22:05:00 -0400

    lab6

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
 const express = require("express")
 const booksRoutes = require("./routes/books")
+const mongoose = require("mongoose")
 
 const app = express()
 
 const SERVER_PORT = 3001
 
+const DB_URL = "mongodb+srv://Meness:B2GWTHPP@cluster0.tamagwf.mongodb.net/db_f2022_comp3123?retryWrites=true&w=majority"
+
+// mongodb+srv://Meness:B2GWTHPP@cluster0.tamagwf.mongodb.net/db_f2022_comp3123.library?retryWrites=true&w=majority
+
+mongoose.connect(DB_URL, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+})
+
 app.use(express.json())
 app.use(express.urlencoded())
 
-
-app.use("/books", booksRoutes)
+app.use("/library/", booksRoutes)
 
 app.route("/")
     .get((req, res) => {
