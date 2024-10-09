7f7004e74fd30a5ed1a5bb6708c65ecfe523b5c5
Pritesh Patel <patelpritesh2484@gmail.com>
2024-10-08 20:28:09 -0400

    Code added

--- routes/books.js
+++ routes/books.js
@@ -1,30 +1,83 @@
 const express = require("express")
+const BookModel = require("../models/books")
+const Book = require("../models/books")
 
 const routes = express.Router()
 
 //Get All Books
 routes.get("/books", (req, res) => {
-    res.send({message: "Get All Books"})
+    // Get all books from MongoDB
+    BookModel.find().then((books) => {
+        res.send(books)
+    }).catch((err) => {
+        res.status(500).send({message: err.message})
+    })
+    //res.send({message: "Get All Books"})
 })
 
 //Add NEW Book
-routes.post("/books", (req, res) => {
-    res.send({message: "Add NEW Book"})
+routes.post("/books", async (req, res) => {
+    const bookData = req.body
+    console.log(bookData)
+    try {
+        // Create a new book instance
+        const book = new BookModel(bookData)
+        // Save the book to MongoDB
+        const newBook = await book.save()
+        res.send(newBook)
+    } catch (err) {
+        res.status(500).send({message: err.message})
+    } 
+  
+    //res.send({message: "Add NEW Book"})
 })
 
 //Update existing Book By Id
-routes.post("/book/:bookid", (req, res) => {
-    res.send({message: "Update existing Book By Id"})
+routes.put("/book/:bookid", (req, res) => {
+
+    // Update book by ID
+
+    BookModel.findByIdAndUpdate(req.params.bookid, req.body, {new: true})
+    .then((book) => {
+        if(book) {
+            res.send(book)
+        } else {
+            res.status(404).send({message: "Book not found"})
+        }
+    }).catch((err) => {
+        res.status(500).send({message: err.message})
+    })
+    //res.send({message: "Update existing Book By Id"})
 })
 
 //Delete Book By ID
 routes.delete("/book/:bookid", (req, res) => {
-    res.send({message: "Delete Book By ID"})
+    BookModel.findByIdAndDelete(req.params.bookid)
+    .then((book) => {
+        if(book) {
+            res.send(book)
+        } else {
+            res.status(404).send({message: "Book not found"})
+        }
+    }).catch((err) => {
+        res.status(500).send({message: err.message})
+    })
+    //res.send({message: "Delete Book By ID"})
 })
 
 //Get Book By ID
 routes.get("/book/:bookid", (req, res) => {
-    res.send({message: "Get Book By ID"})
+    BookModel.findById(req.params.bookid)
+    .then((book) => {
+        if(book) {
+            res.send(book)
+        } else {
+            res.status(404).send({message: "Book not found"})
+        }
+    }).catch((err) => {
+        res.status(500).send({message: err.message})
+    })
+    //res.send({message: "Get Book By ID"})
 })
 
 //Get All Books in sorted order
--- server.js
+++ server.js
@@ -1,5 +1,17 @@
 const express = require("express")
 const booksRoutes = require("./routes/books")
+const mongoose = require("mongoose")
+
+// const DB_CONNECTION_STRING = "mongodb://localhost:27017/books"
+const DB_CONNECTION_STRING = "mongodb+srv://sa:wG0O7vewunLGyYrO@cluster0.7wn4nmp.mongodb.net/week06_lab?retryWrites=true&w=majority&appName=Cluster0"
+mongoose.connect(DB_CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("Connected to MongoDB")
+}).catch((err) => {
+    console.log("Error: ", err)
+})
 
 const app = express()
 
@@ -9,7 +21,7 @@ app.use(express.json())
 app.use(express.urlencoded())
 
 
-app.use("/books", booksRoutes)
+app.use("/api/v1", booksRoutes)
 
 app.route("/")
     .get((req, res) => {
